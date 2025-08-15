f045fcd159eddc561e44a6e5a22e2ade91aa11fb
rmegasari <141827098+rmegasari@users.noreply.github.com>
2025-08-15 11:05:26 +0700

    Update template-preview.js

--- content/template-preview.js
+++ content/template-preview.js
@@ -102,7 +102,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             const linkPayment = `${product.detail.payment_gateway}`;
             const tombolPayment = `
                 <a href="${linkPayment}" class="btn-primary flex items-center justify-center w-full px-8 py-3 rounded-lg font-semibold" onclick="fbq('track', 'InitiateCheckout');">
-                    <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
+                    <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4z"></path></svg>
                     Beli Langsung
                 </a>
             `;
