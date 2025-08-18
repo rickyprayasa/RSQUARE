de4c8df3c4216ecafa9876b8375c72825e03a12b
rmegasari <141827098+rmegasari@users.noreply.github.com>
2025-08-18 08:48:50 +0700

    Update templates-gallery.js

--- templates-gallery.js
+++ templates-gallery.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     try {
         // 1. Ambil urutan produk dari file pengaturan yang baru
-        const indexResponse = await fetch('/content/_index.json'); 
+        const indexResponse = await fetch('content/_index.json'); 
         if (!orderResponse.ok) throw new Error("Gagal memuat file urutan template.");
         
         const orderData = await orderResponse.json();
