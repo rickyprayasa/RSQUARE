44c422f21fb9a0e329c8c0e149611331ad4dbdbc
Ricky Yusar Prayasa <ricky.yusar@gmail.com>
2025-10-02 09:57:21 +0700

    Update featured-templates.js

--- featured-templates.js
+++ featured-templates.js
@@ -54,7 +54,7 @@ async function loadFreeProducts() {
                         <span class="label">GRATIS</span>
                         <h3>🎯 ${product.judul}</h3>
                         <div class="featured-card-description-wrapper">
-                            <a href="${detailLink}" class="btn-primary-small">Lihat Template</a>
+                            <a href="${detailLink}" class="btn-primary-small">Lihat Template onclick="fbq('track', 'InitiateCheckout');"</a>
                         </div>
                     </div>
                 </div>
@@ -101,7 +101,7 @@ async function loadFeaturedProducts() {
                         <span class="label">★ Template Unggulan</span>
                         <h3>🎯 ${product.judul}</h3>
                         <div class="featured-card-description-wrapper">
-                            <a href="${detailLink}" class="btn-primary-small">Lihat Template</a>
+                            <a href="${detailLink}" class="btn-primary-small">Lihat Template onclick="fbq('track', 'InitiateCheckout');"</a>
                         </div>
                     </div>
                 </div>
